02c01f614e2f557247eb8e51b3b8d1641c0c39ff
Rabee93 <rabei_mghrabi@yahoo.com>
2020-07-13 07:06:53 +0100

    MODIFIED comments on how the code should be

--- spec/bowlingspec.js
+++ spec/bowlingspec.js
@@ -9,12 +9,12 @@ describe('Frame', function() {
     frame.roll1();
     expect(frame.playerChoice).toBeLessThan(10);
   });
-  // it("should be less than 0", function(){
+  // it("should not be less than 0", function(){
   //
   //
   //
   //   }
-  //   expect(function () { thermostat.up();}).toThrowError('shouldn't be less than 0');
+  //   expect(function () { frame.roll1();}).toThrowError('shouldn't be less than 0');
 
     // describe('roll2', function(){
     // it('hits a pin and returns from 1 to 10', function(){
